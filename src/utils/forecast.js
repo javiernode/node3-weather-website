d976146775a0c37e809033889eeb17487bef663c
Alvaro Amaya <1800ajamaya@gmail.com>
2021-01-19 16:14:09 -0800

    Add % to Humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,16 +1,5 @@
 const request = require('request')
 
-// const url = 'http://api.weatherstack.com/current?access_key=866f38600b84a3779a02bef4c9a84a37&query=37.8267,-122.4233'
-
-// request({ url: url, json: true }, (error, response) => {
-//  //   console.log(response.body.current)
-//     const temperature = response.body.current.temperature
-//     const feelslike = response.body.current.feelslike
-
-//     console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + temperature + ' degrees out. It feels like ' + feelslike + ' degrees.' )
-// })
-
-
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=866f38600b84a3779a02bef4c9a84a37&query=' + latitude + ',' + longitude + '&units=f'
 
@@ -26,7 +15,7 @@ const forecast = (latitude, longitude, callback) => {
 
             callback(undefined, body.current.weather_descriptions[0] +
                  '. It is currently ' + temperature + ' degrees out. It feels like ' + 
-                 feelslike + ' degrees. Humidity is: ' + humidity)
+                 feelslike + ' degrees. Humidity is: ' + humidity + '%.')
         }
     } )
 
